fc7e22c168ffb1ef1ad4d596dc6b7160d5430a1f
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-08-15 11:10:43 +0300

    old

--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -2,8 +2,8 @@ const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
     await page.goto('http://localhost:8080/');
-    const heading = await page.$('h1');
+    const heading = await page.$('form');
     const text = await heading.textContent();
-    expect(text).toContain('Cookbook');  
+    expect(text).toContain('Welcome to my cooking class');  
   });
   
